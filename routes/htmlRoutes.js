636e42bc2a5256117b39a21c261d5e6e96e03079
Lammie <plkelly@yahoo.com>
2020-03-20 20:48:31 -0700

    Applying changes after git pull.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,60 +1,52 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  app.get("/", function(req, res) {
+module.exports = function (app) {
+  app.get("/", function (req, res) {
     res.render("index");
-    // db.Customer.findAll({}).then(function(data) {
-    //   res.render("index", {
-    //     // customers: data
-    //   });
-    // });
   });
 
-  app.get("/menu", function(req, res) {
-    res.render("menu");
-    // db.Product.findAll({}).then(function(data) {
-      // res.render("menu", {
-      //   products: data,
-      //   orders: data,
-      //   orderitems:data
-      // });
-    // });
+  app.get("/menu", function (req, res) {
+    var orderHistory = {
+      history: []
+    };
+    db.Order.findAll({
+      where: {
+        cid: 3
+      },
+      include: db.OrderItem
+    }).then(function (dbOrder) {
+      for (var i = 0; i < dbOrder.length; i += 1) {
+        for (var j = 0; j < dbOrder[i].OrderItems.length; j++) {
+          var currentOrder = {
+            order: dbOrder[i].dataValues.OrderItems[j].oid,
+            item: dbOrder[i].dataValues.OrderItems[j].item_no,
+            description: dbOrder[i].dataValues.OrderItems[j].prod_name,
+            size: dbOrder[i].dataValues.OrderItems[j].size,
+            price: dbOrder[i].dataValues.OrderItems[j].price,
+            qty: dbOrder[i].dataValues.OrderItems[j].qty
+          }
+          orderHistory.history.push(currentOrder)
+        }
+      }
+      res.render("menu", {drinks:orderHistory});
+      console.log(orderHistory);
+    });
   });
 
-
-  app.get("/checkout", function(req, res) {
+  app.get("/checkout", function (req, res) {
     res.render("checkout");
-    // db.Order.findAll({}).then(function(data) {
-      // res.render("checkout", {
-      //   order: data
-      // });
-    // });
   });
 
-  app.get("/status", function(req, res) {
+  app.get("/status", function (req, res) {
     res.render("status");
-    // db.OrderItem.findAll({}).then(function(data) {
-      // res.render("status", {
-      //   orders: data,
-      //   orderitems: data,
-      //   song: data
-      // });
-    // });
   });
 
-  app.get("/employee", function(req, res) {
+  app.get("/employee", function (req, res) {
     res.render("employee");
-    // db.Song.findAll({}).then(function(data) {
-      // res.render("employee", {
-      //   orders: data,
-      //   orderitems: data,
-      //   song: data
-      // });
-    // });
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
